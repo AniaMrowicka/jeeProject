becea3bf391b69836e544a79febdef720e588dc9
Ania <mrowicka.ania@gmail.com>
2019-08-03 21:31:01 +0200

    zadanie pierwsze

---
+++ src/main/java/ItemService.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class ItemService {
+    public ItemModel getById(int id){
+        return null;
+    }
+    public List<ItemModel> getAll(){
+        return null;
+    }
+}
