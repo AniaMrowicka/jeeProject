08eeb9a8b9bb9839dad32ef6800fe4b4c2818014
Ania <mrowicka.ania@gmail.com>
2019-08-19 20:38:42 +0200

    review changes

--- src/main/java/model/NotesType.java
+++ src/main/java/model/NotesType.java
@@ -1,19 +1,5 @@
 package model;
 
 public enum NotesType {
-    SCAN("scan"), PHOTO("photo"), NOTES("notes"), RECEIPT("receipt");
-
-    public String name;
-
-    NotesType(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    SCAN, PHOTO, NOTES, RECEIPT
 }
